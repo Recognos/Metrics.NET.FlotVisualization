18e53e987811a6c966bf45f4f5047c52da2fb1c9
Eran Ofer <eran.of@gigya-inc.com>
2017-05-30 09:42:38 +0300

    move dropdown Menu Scrollbar script to file

---
+++ app/scripts/DropdownMenu.js
@@ -0,0 +1,12 @@
+﻿(function (win, $) {
+	'use strict';
+
+    $(win).on('load', function () {
+	    $('.dropdown-menu').css('max-height', Math.max(window.innerHeight - 80, 120));
+            });
+
+	$(win).on('resize', function () {
+	    $('.dropdown-menu').css('max-height', Math.max(window.innerHeight - 80, 120));
+            });
+       
+})(this, this.jQuery);
